567371384f5f72f1c6a01d41c993e46796e80b70
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-08 14:25:28 +0530

    nested components

--- app.js
+++ app.js
@@ -4,4 +4,18 @@ function Header() {
 	return <h1>Develop. Preview. Ship. 🚀</h1>;
 }
 
-ReactDOM.render(<Header />, app);
+function Footer() {
+	return <h3>Built by Gayashan with ❤️ </h3>;
+}
+
+function HomePage() {
+	return (
+		<>
+			<Header />
+			<p>Welcome to ReactJs.</p>
+			<Footer />
+		</>
+	);
+}
+
+ReactDOM.render(<HomePage />, app);
