db0dada1c10acd6fc93b0176e35c77ced139fbf0
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-08 14:31:12 +0530

    add lorem ipsum text

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ function Header() {
 	return <h1>Develop. Preview. Ship. 🚀</h1>;
 }
 
+function Article() {
+	return (
+		<div id='first-article'>
+			<h2 className='title'>What is Lorem Ipsum</h2>
+			<p className='content'>
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industry's standard dummy text ever
+				since the 1500s, when an unknown printer took a galley of type and
+				scrambled it to make a type specimen book. It has survived not only five
+				centuries, but also the leap into electronic typesetting, remaining
+				essentially unchanged.
+			</p>
+		</div>
+	);
+}
+
 function Footer() {
-	return <h3>Built by Gayashan with ❤️ </h3>;
+	return <h5>Built by Gayashan with ❤️ </h5>;
 }
 
 function HomePage() {
 	return (
 		<>
 			<Header />
-			<p>Welcome to ReactJs.</p>
+			<Article />
 			<Footer />
 		</>
 	);
